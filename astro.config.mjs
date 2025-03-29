6f37b5e79d2a4f06ec67baf601f12247df100060
Vinayoo4 <vini.rao.oo4+git@gmail.com>
2025-03-30 03:35:39 +0530

    Initial commit or update PaymentForm component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  devOptions:{
+    devToolbar: false,
+  },
   integrations: [tailwind()],
   site: 'https://github.com/Vinayoo4/Aquaflow'
 });
